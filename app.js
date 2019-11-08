a622748e8575fb2e7fb2e01fdeafd41d47f7526c
Aamir Mahmood <mohdaamir8182@gmail.com>
2019-11-08 23:23:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product' , (req, res, next) => {
-    console.log("In the add product");
-    res.send('<html><head><title>Add Product</title></head><body><h1>Add new Product</h1></body></html>');
+    res.send('<html><head><title>Add Product</title></head><body><h1>Add new Product</h1><br><br><form action="/product" method="POST"><input tupe="text" name="title" /><button type="submit">Add Product</button></form></body></html>');
+});
+
+app.use('/product', (req , res , next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log("In the root!");
     res.send('<html><head><title>Root directory</title></head><body><h1>In the root directory.</h1></body></html>');
 });
 
