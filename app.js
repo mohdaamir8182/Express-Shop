31651d940e779e3d5871331b7c2b40bba797a935
Aamir Mahmood <mohdaamir8182@gmail.com>
2019-11-08 20:40:06 +0500

    routes added

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req , res , next)=>{
-    console.log("In the middleware!");
-    next();
+app.use('/add-product' , (req, res, next) => {
+    console.log("In the add product");
+    res.send('<html><head><title>Add Product</title></head><body><h1>Add new Product</h1></body></html>');
 });
 
-app.use((req, res, next) => {
-    console.log("In the nextttt middleware!")
+app.use('/', (req, res, next) => {
+    console.log("In the root!");
+    res.send('<html><head><title>Root directory</title></head><body><h1>In the root directory.</h1></body></html>');
 });
 
 app.listen(3000);
